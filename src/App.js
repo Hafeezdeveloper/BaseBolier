d213252fb631e08c75cd22f23e20f980c9cb0a6e
Hafeezdeveloper <Hafeez.hussain2003@gmail.com>
2023-04-02 02:53:02 +0500

    Mode2basePlate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+      <>
+      </>
+  );
+}
+
+export default App;
